8e97d513f36d873a37e6cf4e15e9f410bd386817
lucassodresa <lucas.sodresa@gmail.com>
2024-02-22 19:54:14 +0000

    update actions

--- sanity/sanity.cli.ts
+++ sanity/sanity.cli.ts
@@ -3,6 +3,6 @@ import {defineCliConfig} from 'sanity/cli'
 export default defineCliConfig({
   api: {
     projectId: 'jlf6c7o6',
-    dataset: 'production'
-  }
+    dataset: 'production',
+  },
 })
--- sanity/sanity.config.ts
+++ sanity/sanity.config.ts
@@ -6,12 +6,9 @@ import {schemaTypes} from './schemaTypes'
 export default defineConfig({
   name: 'default',
   title: 'blog',
-
   projectId: 'jlf6c7o6',
   dataset: 'production',
-
   plugins: [structureTool(), visionTool()],
-
   schema: {
     types: schemaTypes,
   },
