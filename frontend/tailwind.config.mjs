cc2dce94912516ef4e6f96a5932430b81e94421d
lucassodresa <lucas.sodresa@gmail.com>
2024-02-26 19:58:27 +0000

    refactor to use flowbite

--- frontend/tailwind.config.mjs
+++ frontend/tailwind.config.mjs
@@ -2,13 +2,28 @@ import defaultTheme from "tailwindcss/defaultTheme";
 
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+  content: [
+    "./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}",
+    "./node_modules/flowbite/**/*.js",
+  ],
   theme: {
     extend: {
-      fontFamily: {
-        sans: ["Mona Sans", ...defaultTheme.fontFamily.sans],
+      colors: {
+        primary: {
+          50: "#eff6ff",
+          100: "#dbeafe",
+          200: "#bfdbfe",
+          300: "#93c5fd",
+          400: "#60a5fa",
+          500: "#3b82f6",
+          600: "#2563eb",
+          700: "#1d4ed8",
+          800: "#1e40af",
+          900: "#1e3a8a",
+          950: "#172554",
+        },
       },
     },
   },
-  plugins: [],
+  plugins: [require("flowbite/plugin"), require('flowbite-typography')],
 };
