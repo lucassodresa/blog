10994c9261ff6547f26277b00efd746a7665f6f1
lucassodresa <lucas.sodresa@gmail.com>
2024-03-11 01:13:04 +0000

    add code highlight on blog post

--- sanity/sanity.config.ts
+++ sanity/sanity.config.ts
@@ -2,14 +2,14 @@ import {defineConfig} from 'sanity'
 import {structureTool} from 'sanity/structure'
 import {visionTool} from '@sanity/vision'
 import {schemaTypes} from './schemaTypes'
-import { codeInput } from '@sanity/code-input'
+import {codeInput} from '@sanity/code-input'
 
 export default defineConfig({
   name: 'default',
   title: 'blog',
   projectId: 'jlf6c7o6',
   dataset: 'production',
-  plugins: [structureTool(), visionTool(), codeInput()],
+  plugins: [codeInput(), structureTool(), visionTool()],
   schema: {
     types: schemaTypes,
   },
