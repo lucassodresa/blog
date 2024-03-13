16a3b8ab91ec29210d296275ace072dd4ef27862
lucassodresa <lucas.sodresa@gmail.com>
2024-03-13 23:31:00 +0000

    solve build issue

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,21 @@
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
 import { sanityIntegration } from "@sanity/astro";
 import tailwind from "@astrojs/tailwind";
-
+import { join } from "path";
 
 export default defineConfig({
-  site: 'https://blog.lucassodresa.com',
-  integrations: [tailwind(), sanityIntegration({
-    projectId: "jlf6c7o6",
-    dataset: "production",
-    useCdn: false
-  }),  ]
+  site: "https://blog.lucassodresa.com",
+  integrations: [
+    tailwind(),
+    sanityIntegration({
+      projectId: "jlf6c7o6",
+      dataset: "production",
+      useCdn: false,
+    }),
+  ],
+  resolve: {
+    alias: {
+      "@": join(__dirname, "src"),
+    },
+  },
 });
